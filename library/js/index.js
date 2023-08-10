d2cbd476177b3716f3bede1ee964d3c21925a8b7
Danila <AngelofDest1ny@yandex.ru>
2023-08-10 20:54:00 +0300

    fix: add opacity to burger

--- library/js/index.js
+++ library/js/index.js
@@ -1,11 +1,19 @@
 'use strict';
+//TASK 1
 console.log(`
 Верстка валидная + 10;
 Верстка семантическая + 16;
 Верстка соответствует макету +54;
 Выполненый общие требования к верстке +20;
 `);
+//TASK 2
+console.log(`
+1.Вёрстка соответствует макету. Ширина экрана 768px +26
+2.Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12
+3.На ширине экрана 768рх реализовано адаптивное меню +12
+`);
 
+//---------------------------Burger menu---------------------------
 const burgerMenu = document.querySelector('.header__burger');
 const headerList = document.querySelector('.header__list');
 const navItems = document.querySelectorAll('.header__list-item');
@@ -21,7 +29,11 @@ function close() {
 }
 navItems.forEach(item => item.addEventListener('click', close));
 document.addEventListener('click', e => {
-  if((!burgerMenu.classList.contains('active') || !headerList.contains(e.target)) && !burgerMenu.contains(e.target)) {
-    close()
+  if (
+    (!burgerMenu.classList.contains('active') ||
+      !headerList.contains(e.target)) &&
+    !burgerMenu.contains(e.target)
+  ) {
+    close();
   }
 });
