51d236e1ac8a5bce9d3a3d823bd43d4b627c6d21
Danila <AngelofDest1ny@yandex.ru>
2023-08-31 19:50:24 +0300

    feat: add slider at section favorites

--- library/js/index.js
+++ library/js/index.js
@@ -131,3 +131,190 @@ slidesNavigation.forEach((item, index) => {
     activeSlide(index);
   });
 });
+
+//---------------------------Slider at Favorites---------------------------
+
+const labelsOfWeather = document.querySelectorAll('.favorites-label');
+const favoritesContainer = document.querySelectorAll('.favorites__container-card');
+const btnFavoritesCard = document.querySelectorAll('.favorites__card-button');
+const winter = [
+  {
+    titleBook: 'The Book Eaters',
+    bookAuthor: 'By Sunyi Dean',
+    text: `An unusual sci-fi story about a book eater woman who tries desperately to save her dangerous mind-eater son from tradition and certain death. Complete with dysfunctional family values, light Sapphic romance, and a strong, complex protagonist. Not for the faint of heart.`,
+    buy: false,
+    img: './assets/img/winter/sunyi_dean.jpg',
+  },
+  {
+    titleBook: 'Cackle',
+    bookAuthor: 'By Rachel Harrison',
+    text: `Are your Halloween movies of choice The Witches of Eastwick and Practical Magic? Look no further than here - where a woman recovering from a breakup moves to a quaint town in upstate New York and befriends a beautiful witch.`,
+    buy: false,
+    img: './assets/img/winter/rachel_harrison.jpg',
+  },
+  {
+    titleBook: 'Dante: Poet of the Secular World',
+    bookAuthor: 'By Erich Auerbach',
+    text: `Auerbach's engaging book places the 'Comedy' within the tradition of epic, tragedy, and philosophy in general, arguing for Dante's uniqueness as one who raised the individual and his drama of soul into something of divine significance—an inspired introduction to Dante's main themes.`,
+    buy: false,
+    img: './assets/img/winter/dante.jpg',
+  },
+  {
+    titleBook: 'The Last Queen',
+    bookAuthor: 'By Clive Irving',
+    text: `A timely and revelatory new biography of Queen Elizabeth (and her family) exploring how the Windsors have
+  evolved and thrived as the modern world has changed around them.`,
+    buy: false,
+    img: './assets/img/winter/queen.jpg',
+  },
+];
+const spring = [
+  {
+    titleBook: 'The Body',
+    bookAuthor: 'By Stephen King',
+    text: `Powerful novel that takes you back to a nostalgic time, exploring both the beauty and danger and loss of
+    innocence that is youth.`,
+    buy: false,
+    img: './assets/img/spring/stephen_king.jpg',
+  },
+  {
+    titleBook: 'Carry: A Memoir of Survival on Stolen Land',
+    bookAuthor: 'By Toni Jenson',
+    text: `This memoir about the author's relationship with gun violence feels both expansive and intimate, resulting
+    in a lyrical indictment of the way things are.`,
+    buy: false,
+    img: './assets/img/spring/toni_jenson.jpg',
+  },
+  {
+    titleBook: 'Days of Distraction',
+    bookAuthor: 'By Alexandra Chang',
+    text: `A sardonic view of Silicon Valley culture, a meditation on race, and a journal of displacement and
+    belonging, all in one form-defying package of spare prose.`,
+    buy: false,
+    img: './assets/img/spring/alexandra_chang.jpg',
+  },
+  {
+    titleBook: 'Dominicana',
+    bookAuthor: 'By Angie Cruz',
+    text: `A fascinating story of a teenage girl who marries a man twice her age with the promise to bring her to
+    America. Her marriage is an opportunity for her family to eventually immigrate. For fans of Isabel Allende
+    and Julia Alvarez.`,
+    buy: false,
+    img: './assets/img/spring/angie_cruz.jpg',
+  },
+];
+const summer = [
+  {
+    titleBook: 'Crude: A Memoir',
+    bookAuthor: 'By Pablo Fajardo & ​​Sophie Tardy-Joubert',
+    text: `Drawing and color by Damien Roudeau | This book illustrates the struggles of a group of indigenous
+    Ecuadoreans as they try to sue the ChevronTexaco company for damage their oil fields did to the Amazon and
+    her people`,
+    buy: false,
+    img: './assets/img/summer/palbo_fajardo.jpg',
+  },
+  {
+    titleBook: 'Let My People Go Surfing',
+    bookAuthor: 'By Yvon Chouinard',
+    text: `Chouinard—climber, businessman, environmentalist—shares tales of courage and persistence from his
+    experience
+    of founding and leading Patagonia, Inc. Full title: Let My People Go Surfing: The Education of a Reluctant
+    Businessman, Including 10 More Years of Business Unusual.`,
+    buy: false,
+    img: './assets/img/summer/yvon_chouinard.jpg',
+  },
+  {
+    titleBook: 'The Octopus Museum: Poems',
+    bookAuthor: 'By Brenda Shaughnessy',
+    text: `This collection of bold and scathingly beautiful feminist poems imagines what comes after our current age
+    of
+    environmental destruction, racism, sexism, and divisive politics.`,
+    buy: false,
+    img: './assets/img/summer/brenda_shaughnnesy.jpg',
+  },
+  {
+    titleBook: 'Shark Dialogues: A Novel',
+    bookAuthor: 'By Kiana Davenport',
+    text: `An epic saga of seven generations of one family encompasses the tumultuous history of Hawaii as a Hawaiian
+    woman gathers her four granddaughters together in an erotic tale of villains and dreamers, queens and
+    revolutionaries, lepers and healers.`,
+    buy: false,
+    img: './assets/img/summer/kiana_davenport.jpg',
+  },
+];
+const autumn = [
+  {
+    titleBook: 'Casual Conversation',
+    bookAuthor: 'By Renia White',
+    text: `White's impressive debut collection takes readers through and beyond the concepts of conversation and the
+    casual - both what we say to each other and what we don't, examining the possibilities around how we
+    construct and communicate identity.`,
+    buy: false,
+    img: './assets/img/autumn/riana_white.jpg',
+  },
+  {
+    titleBook: 'The Great Fire',
+    bookAuthor: 'By Lou Ureneck',
+    text: `The harrowing story of an ordinary American and a principled Naval officer who, horrified by the burning
+    of
+    Smyrna, led an extraordinary rescue effort that saved a quarter of a million refugees from the Armenian
+    Genocide`,
+    buy: false,
+    img: './assets/img/autumn/lou_ureneck.jpg',
+  },
+  {
+    titleBook: 'Rickey: The Life and Legend',
+    bookAuthor: 'By Howard Bryant',
+    text: `With the fall rolling around, one can't help but think of baseball's postseason coming up! And what better
+    way to prepare for it than reading the biography of one of the game's all-time greatest performers, the
+    Man
+    of Steal, Rickey Henderson?`,
+    buy: false,
+    img: './assets/img/autumn/howard_bryant.jpg',
+  },
+  {
+    titleBook: 'Slug: And Other Stories',
+    bookAuthor: 'By Megan Milks',
+    text: `Exes Tegan and Sara find themselves chained together by hairballs of codependency. A father and child
+    experience the shared trauma of giving birth to gods from their wounds.`,
+    buy: false,
+    img: './assets/img/autumn/megan_milks.jpg',
+  },
+];
+function fadeIn(elem, timeout, display) {
+  elem.style.opacity = 0;
+  elem.style.display = display || 'block';
+  elem.style.transition = `opacity ${timeout}ms`;
+  setTimeout(() => {
+    elem.style.opacity = 1;
+  }, 10);
+}
+function fadeOut(elem, timeout) {
+  elem.style.opacity = 1;
+  elem.style.transition = `opacity ${timeout}ms`;
+  elem.style.opacity = 0;
+  setTimeout(() => {
+    elem.style.display = 'none';
+  }, timeout);
+}
+labelsOfWeather.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    activeLabel(index);
+    showContainerOfBooks(index, 1000);
+  });
+});
+function showContainerOfBooks(index, timeout) {
+  favoritesContainer.forEach((item, i) => {
+    fadeOut(item, timeout);
+  });
+  setTimeout(() => {
+    fadeIn(favoritesContainer[index], timeout, 'grid');
+  }, timeout);
+}
+showContainerOfBooks(0);
+function activeLabel(index) {
+  labelsOfWeather.forEach(item =>
+    item.classList.remove('favorites-label_active'),
+  );
+  labelsOfWeather[index].classList.add('favorites-label_active');
+}
